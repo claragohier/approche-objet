c573d0cf6ca8ef3a2933610a1ae955861a74f7ee
claragohier <clara.gohier@gmail.com>
2021-10-07 13:11:40 +0200

    tp 4

---
+++ src/utils/ConversionNombre.java
@@ -0,0 +1,17 @@
+package utils;
+
+public class ConversionNombre {
+
+	public static void main(String[] args) {
+		
+		String chaine = "12";
+		
+		int nb = Integer.parseInt(chaine);
+		
+		int a = 5;
+		int b = 1;
+		
+		System.out.println(Math.max(a, b));
+	}
+
+}
