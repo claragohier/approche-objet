f1dd8833b67d5a3823e51607c1ae5dd68b75ff14
claragohier <clara.gohier@gmail.com>
2021-10-07 13:17:01 +0200

    tp encapsulation

---
+++ src/fr/diginamic/banque/entites/Comptes.java
@@ -0,0 +1,12 @@
+package fr.diginamic.banque.entites;
+
+public class Comptes {
+
+	int numeroCompte;
+	int soldeCompte;
+	
+	Comptes(int numeroCompte, int soldeCompte){
+		this.numeroCompte = numeroCompte;
+		this.soldeCompte = soldeCompte;
+	}
+}
---
+++ src/fr/diginamic/banque/entites/TestBanque.java
@@ -0,0 +1,17 @@
+package fr.diginamic.banque.entites;
+
+public class TestBanque {
+
+	public static void main(String[] args) {
+
+		Comptes compte = new Comptes(1, 500);
+		
+		// on ne peut pas lire l'objet
+		System.out.println(compte);
+	
+		// on doit lire les valeurs passées en attributs
+		System.out.println(compte.numeroCompte);
+		System.out.println(compte.soldeCompte);
+	}
+
+}
